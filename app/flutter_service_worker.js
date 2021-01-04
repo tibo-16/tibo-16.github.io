f1730ba76849a3c70e04ccbe2c1048835aad81ae
Tobias Emrich <tobiemrich@gmail.com>
2021-01-04 16:47:27 +0100

    forth try to fix view for iPhone 6

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "fed776564e9a1dc935c3c71c3ebcc2a3",
-"index.html": "b11c5575230a03606d7801af588d4722",
-"/": "b11c5575230a03606d7801af588d4722",
-"main.dart.js": "6664943dab30af2d9c3b303da58d954b",
+"index.html": "91b94fa4c47febaf7446c7fe7daa3e8b",
+"/": "91b94fa4c47febaf7446c7fe7daa3e8b",
+"main.dart.js": "538189d2db5f4568fee460061291d64a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
